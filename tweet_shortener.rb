d24f0d91ee94d67bdcb84cec3c93de4697553fb8
Faraz Hanif <hfaraz745@gmail.com>
2018-04-20 04:28:29 +0000

    automatically backed up by learn

---
+++ tweet_shortener.rb
@@ -0,0 +1,26 @@
+def word_substituter(tweet)
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+
+    tweetarr = tweet.split(" ")
+    tweetarr.collect do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+
+      end
+    end
+    tweetarr.join(" ")
+end  
+
+
